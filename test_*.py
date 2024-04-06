4def0a37b8535e1e2f3d457fc555177ccba0b939
V-SANT <vsantander@fi.uba.ar>
2024-04-06 20:07:46 -0300

    Nuevas funciones para verificar tests

--- test_*.py
+++ test_*.py
@@ -1,5 +1,5 @@
 from main import obtener_datos
-from tp1_greedy import obtener_suma_ponderada
+from tp1_greedy import *
 
 def test_ejemplo_catedra_1():
     assert obtener_suma_ponderada(obtener_datos('./archivos_pruebas_tp1/10.txt')) == 309600
@@ -24,17 +24,42 @@ def test_ejemplo_catedra_7():
 
 def test_grupo_1():
     # Todos con el mismo peso deberia ejecutarse las de menor duracion al principio
-    batallas = [(3, 1), (2, 1), (4, 1)] 
+    batallas = [(3, 1), (2, 1), (4, 1)]
+    assert obtener_batallas_ordenadas(batallas) == [(2, 1), (3, 1), (4, 1)]
     assert obtener_suma_ponderada(batallas) == 16
 
 def test_grupo_2():
     # Al tener distintos pesos ahora usamos la relacion de peso/tiempo. En caso de igualdad da lo mismo cual se ejecuta
     batallas = [(3, 3), (2, 1), (4, 2)] 
+    assert obtener_batallas_ordenadas(batallas) == [(3, 3), (2, 1), (4, 2)]
     assert obtener_suma_ponderada(batallas) == 32
 
 def test_grupo_3():
     # En este caso el orden de las batallas seran: b2,b3,b1
-    batallas = [(5, 1), (3, 5), (4, 3)] 
+    batallas = [(5, 1), (3, 5), (4, 3)]
+    assert obtener_batallas_ordenadas(batallas) == [(3, 5), (4, 3), (5, 1)]
     assert obtener_suma_ponderada(batallas) == 48
+
+def test_grupo_4():
+    batallas = [(2, 5), (3, 9), (4, 11)] 
+    assert obtener_batallas_ordenadas(batallas) == [(3, 9), (4, 11), (2, 5)]
+    assert obtener_suma_ponderada(batallas) == 149
+
+    assert obtener_batallas_ordenadas_por_peso(batallas) == [(4, 11), (3, 9), (2, 5)]
+    assert obtener_suma_ponderada_por_peso(batallas) == 152
+    
+    assert obtener_batallas_ordenadas_por_tiempo(batallas) == [(2, 5), (3, 9), (4, 11)]
+    assert obtener_suma_ponderada_por_tiempo(batallas) == 154
+
+def test_grupo_5():
+    batallas = [(5, 2), (9, 3), (11, 4)]  
+    assert obtener_suma_ponderada(batallas) == 149
+    assert obtener_batallas_ordenadas(batallas) == [(5, 2), (11, 4), (9, 3)]
+
+    assert obtener_suma_ponderada_por_peso(batallas) == 154
+    assert obtener_batallas_ordenadas_por_peso(batallas) == [(11, 4), (9, 3), (5, 2)]
+    
+    assert obtener_suma_ponderada_por_tiempo(batallas) == 152
+    assert obtener_batallas_ordenadas_por_tiempo(batallas) == [(5, 2), (9, 3), (11, 4)]
     
 
--- tp1_greedy.py
+++ tp1_greedy.py
@@ -15,3 +15,56 @@ def obtener_suma_ponderada(batallas):
         suma_ponderada += suma_parcial*peso_batalla
 
     return suma_ponderada
+
+def obtener_batallas_ordenadas(batallas):
+    regla = lambda batalla: obtener_ratio_batalla(batalla)
+    return sorted(batallas, key=regla, reverse=True)
+
+#------------------------------------------------------------
+def obtener_peso_batalla(batalla):
+    _, peso_batalla = batalla
+    return peso_batalla
+
+def obtener_suma_ponderada_por_peso(batallas):
+    regla = lambda batalla: obtener_peso_batalla(batalla)
+    batallas_ordenadas = sorted(batallas, key=regla, reverse=True)
+    
+    suma_ponderada = 0
+    suma_parcial = 0
+    
+    for batalla in batallas_ordenadas:
+        tiempo_batalla,peso_batalla = batalla
+        suma_parcial += tiempo_batalla
+        suma_ponderada += suma_parcial*peso_batalla
+
+    return suma_ponderada
+
+def obtener_batallas_ordenadas_por_peso(batallas):
+    regla = lambda batalla: obtener_peso_batalla(batalla)
+    return sorted(batallas, key=regla, reverse=True)
+
+#-------------------------------------------------------------
+def obtener_tiempo_batalla(batalla):
+    tiempo_batalla, _ = batalla
+    return tiempo_batalla
+
+def obtener_suma_ponderada_por_tiempo(batallas):
+    regla = lambda batalla: obtener_tiempo_batalla(batalla)
+    batallas_ordenadas = sorted(batallas, key=regla)
+    
+    suma_ponderada = 0
+    suma_parcial = 0
+    
+    for batalla in batallas_ordenadas:
+        tiempo_batalla,peso_batalla = batalla
+        suma_parcial += tiempo_batalla
+        suma_ponderada += suma_parcial*peso_batalla
+
+    return suma_ponderada
+
+def obtener_batallas_ordenadas_por_tiempo(batallas):
+    regla = lambda batalla: obtener_tiempo_batalla(batalla)
+    return sorted(batallas, key=regla)
+
+
+
