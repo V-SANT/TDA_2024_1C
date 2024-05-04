06bf32495dda8fede43dc27f12cc74a18cb130e8
TomasGalian <tomiezeg@gmail.com>
2024-05-04 00:48:27 -0300

    Agrego tests de grupo

--- TP2/test_*.py
+++ TP2/test_*.py
@@ -1,12 +1,16 @@
 from main import *
 from ataques_optimos import *
 from constantes_tp2 import *
+import time
 
 def test_ejemplo_catedra_1():
     ruta_archivo = './archivos_pruebas/'+ARCHIVOS_DE_PRUEBA[0]
     ruta_resultados = './archivos_pruebas/'+RESULTADOS_ESPERADOS
     n, arribos, valores_recarga = leer_archivo_prueba(ruta_archivo)
+    inicio_tiempo = time.time()  # Iniciar medición de tiempo
     resultado = ataques_optimos(n, arribos, valores_recarga)
+    fin_tiempo = time.time()  # Finalizar medición de tiempo
+    print(fin_tiempo - inicio_tiempo)  # Calcular tiempo total
     assert (resultado[0] == leer_resultados_esperados(ruta_resultados)[ARCHIVOS_DE_PRUEBA[0]][0])
 
 def test_ejemplo_catedra_2():
@@ -72,4 +76,65 @@ def test_ejemplo_catedra_10():
     resultado = ataques_optimos(n, arribos, valores_recarga)
     assert (resultado[0] == leer_resultados_esperados(ruta_resultados)[ARCHIVOS_DE_PRUEBA[9]][0])
 
+def test_ejemplo_catedra_10():
+    ruta_archivo = './archivos_pruebas/'+ARCHIVOS_DE_PRUEBA[9]
+    ruta_resultados = './archivos_pruebas/'+RESULTADOS_ESPERADOS
+    n, arribos, valores_recarga = leer_archivo_prueba(ruta_archivo)
+    resultado = ataques_optimos(n, arribos, valores_recarga)
+    assert (resultado[0] == leer_resultados_esperados(ruta_resultados)[ARCHIVOS_DE_PRUEBA[9]][0])
+
+def test_grupo_1_agrasiva():
+    n = 2
+    arribos = [5,10]
+    valores_recarga = [3,4]
+    resultado = ataques_optimos(n, arribos, valores_recarga)
+    assert (resultado[0] == 6)
+
+def test_grupo_2_conservadora():
+    n = 2
+    arribos = [5,10]
+    valores_recarga = [3,10]
+    resultado = ataques_optimos(n, arribos, valores_recarga)
+    assert (resultado[0] == 10)
     
+def test_grupo_3_valores_recarga_bajos():
+    # En este caso tenemos varios escenarios para llegar al optimal:
+    # 1) Podriamos atacar siempre con 1
+    # 2.a) Podriamos atacar, cargar y atacar
+    # 2.b) Podriamos cargar, atcar y atacar
+    # 3) Podriamos cargar,cargar y atacar
+    n = 3
+    arribos = [10, 20, 30]
+    valores_recarga = [1, 2, 3]
+    resultado = ataques_optimos(n, arribos, valores_recarga)
+    assert (resultado[0] == 3)  
+
+def test_grupo_4_llegadas_constantes():
+    # En este caso ocurre lo mismo de tener varios casos posibles de ejecución
+    n = 5
+    arribos = [10, 10, 10, 10, 10]
+    valores_recarga = [5, 10, 15, 20, 25]
+    resultado = ataques_optimos(n, arribos, valores_recarga)
+    assert (resultado[0] == 25)  
+
+def test_grupo_5_llegadas_fluctuantes():
+    n = 5
+    arribos = [5, 20, 5, 20, 5]
+    valores_recarga = [10, 15, 20, 25, 30]
+    resultado = ataques_optimos(n, arribos, valores_recarga)
+    assert (resultado[0] == 35)  
+
+def test_grupo_6_llegadas_decrecientes():
+    n = 5
+    arribos = [25, 20, 15, 10, 5]
+    valores_recarga = [5, 10, 15, 20, 25]
+    resultado = ataques_optimos(n, arribos, valores_recarga)
+    assert (resultado[0] == 25)  
+
+def test_grupo_7_llegadas_y_recargas_iguales():
+    n = 5
+    arribos = [5, 10, 15, 20, 25]
+    valores_recarga = [5, 10, 15, 20, 25]
+    resultado = ataques_optimos(n, arribos, valores_recarga)
+    assert (resultado[0] == 25)  
+
