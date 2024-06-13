83672a2f9752d010d5517bf964f090ca96ae0c1e
Azul Fuentes <azul.fuentes@amaze.co>
2024-06-12 23:43:57 -0300

    implementa insertar ordenado con buqueda binaria

--- TP3/gd.py
+++ TP3/gd.py
@@ -1,22 +1,29 @@
+
 def insertar_ordenado(grupos, grupo_minimo):
-    grupos_aux = []
-    encontrado = False
-
-    for grupo in grupos:
-        if grupo_minimo[1]**2 <= grupo[1]**2 and not encontrado:
-            grupos_aux.append(grupo_minimo)
-            encontrado = True
-        grupos_aux.append(grupo)
+    def valor_cuadrado(grupo):
+        return grupo[1]**2
     
-    if not encontrado:
-        grupos_aux.append(grupo_minimo)
+    def busqueda_binaria(grupos, grupo_minimo):
+        valor_minimo = valor_cuadrado(grupo_minimo)
+        izquierda, derecha = 0, len(grupos)
+        while izquierda < derecha:
+            medio = (izquierda + derecha) // 2
+            if valor_cuadrado(grupos[medio]) < valor_minimo:
+                izquierda = medio + 1
+            else:
+                derecha = medio
+        return izquierda
+    
+    posicion = busqueda_binaria(grupos, grupo_minimo)
+    grupos.insert(posicion, grupo_minimo)
+    
+    return grupos
 
-    return grupos_aux
+
 
 def p_opt_tribu_agua_gd(maestros, habilidades, k):
-    tuplas_maestros = list(zip(maestros, habilidades))
+    tuplas_maestros = list(zip(maestros, habilidades)) 
     maestros_ordenados = sorted(tuplas_maestros, key=lambda x: x[1], reverse=True)
-
     grupos = [([], 0) for _ in range(k)]
 
     for maestro in maestros_ordenados:
@@ -30,8 +37,4 @@ def p_opt_tribu_agua_gd(maestros, habilidades, k):
     grupos_finales = [x[0] for x in grupos]
     coeficiente = sum(x[1]**2 for x in grupos)
 
-    print("grupos finales:", grupos_finales)
-
     return grupos_finales, coeficiente
-
-
